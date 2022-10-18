574b505a431847f3b71bfbe0c9bd27ef43f70d6f
Dylan Hamersztein <dylanhamersztein@gmail.com>
2022-10-18 21:49:54 +0100

    Light refactor.

--- server/src/test/kotlin/com/pearlbailey/pearlbaileyhighschool/common/EndpointTestParent.kt
+++ server/src/test/kotlin/com/pearlbailey/pearlbaileyhighschool/common/EndpointTestParent.kt
@@ -1,7 +1,6 @@
 package com.pearlbailey.pearlbaileyhighschool.common
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.pearlbailey.pearlbaileyhighschool.spring.web.error.ErrorResponseFactory
 import org.mockito.Answers.CALLS_REAL_METHODS
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.mock.mockito.MockBean
