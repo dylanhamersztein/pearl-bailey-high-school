80eb5dc7c427416c64a96e39256f7723aec409b5
Dylan Hamersztein <dylanhamersztein@gmail.com>
2022-10-24 15:37:05 +0100

    Fix flyway.

--- course-milestone-manager/build.gradle.kts
+++ course-milestone-manager/build.gradle.kts
@@ -17,6 +17,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+    implementation("org.flywaydb:flyway-core")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
