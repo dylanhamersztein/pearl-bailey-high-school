4cc20f4cf054c17c3c45c95df1ad6521559a59f7
Dylan Hamersztein <dylanhamersztein@users.noreply.github.com>
2022-10-24 13:30:48 +0100

    Update Kubernetes resource definition files for new microservice architecture. (#24)
    * Update Kubernetes resource definition files for new microservice architecture. Update microservice configuration files where appropriate.
    
    * Fix DB names in microservice configmaps.
    
    * Fix hostnames in configmaps.

--- course-milestone-manager/src/main/kotlin/com/pearlbailey/PearlBaileyCourseMilestoneManager.kt
+++ course-milestone-manager/src/main/kotlin/com/pearlbailey/PearlBaileyCourseMilestoneManager.kt
@@ -4,9 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class PearlBaileyDepartmentManager
+class PearlBaileyCourseMilestoneManager
 
 fun main(args: Array<String>) {
     @Suppress("SpreadOperator")
-    runApplication<PearlBaileyDepartmentManager>(*args)
+    runApplication<PearlBaileyCourseMilestoneManager>(*args)
 }
